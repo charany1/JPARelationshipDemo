e9e3951ee31f0802c4e86ff1c80e55d02512fe94
yougeshwer Dan Charan <danchara@adobe.com>
2019-06-28 09:17:50 +0530

    adds Laptop in User as OneToOne mapping

--- src/main/java/com/blogofyogi/JPARelationshipDemo/model/User.java
+++ src/main/java/com/blogofyogi/JPARelationshipDemo/model/User.java
@@ -2,10 +2,7 @@ package com.blogofyogi.JPARelationshipDemo.model;
 
 import lombok.Data;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Data
 @Entity
@@ -15,4 +12,7 @@ public class User {
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Integer id;
     private String name;
+
+    @OneToOne
+    private Laptop laptop;
 }
