5b88938386e6a2d7d2733d7abc70e121666d11cc
yougeshwer Dan Charan <danchara@adobe.com>
2019-06-28 09:11:36 +0530

    Created Laptop and User entity

---
+++ src/main/java/com/blogofyogi/JPARelationshipDemo/JpaRelationshipDemoApplication.java
@@ -0,0 +1,13 @@
+package com.blogofyogi.JPARelationshipDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaRelationshipDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpaRelationshipDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/blogofyogi/JPARelationshipDemo/JpaRelationshipDemoApplicationTests.java
@@ -0,0 +1,16 @@
+package com.blogofyogi.JPARelationshipDemo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class JpaRelationshipDemoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
